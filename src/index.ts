f7cc30cc912dead07d1790874015bfbfed15679d
Ian McCausland <imccausl@gmail.com>
2024-02-12 08:15:12 -0500

    chore: add lint and typecheck to CI

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-import type { ESLint } from 'eslint';
+import type { ESLint } from 'eslint'
 
 const config: ESLint.ConfigData = {
-  extends: [  
-    '@imccausl/eslint-config/base',
-    '@imccausl/eslint-config/react',
-    '@imccausl/eslint-config/test',
-  ],
-  rules: {}
+    extends: [
+        '@imccausl/eslint-config/base',
+        '@imccausl/eslint-config/react',
+        '@imccausl/eslint-config/test',
+    ],
+    rules: {},
 }
 
 export = config
--- src/react/index.ts
+++ src/react/index.ts
@@ -1,43 +1,44 @@
-import type { ESLint } from 'eslint';
-import { doesModuleExist } from '../util';
+import { doesModuleExist } from '../util'
+
+import type { ESLint } from 'eslint'
 
 const plugins: ESLint.ConfigData['plugins'] = []
 const extensions: ESLint.ConfigData['extends'] = []
 
 if (doesModuleExist('react')) {
-  plugins.push('react')
-  extensions.push('plugin:react/recommended')
+    plugins.push('react')
+    extensions.push('plugin:react/recommended')
 }
 
 if (doesModuleExist('react-hooks')) {
-  plugins.push('react-hooks')
-  extensions.push('plugin:react-hooks/recommended')
+    plugins.push('react-hooks')
+    extensions.push('plugin:react-hooks/recommended')
 }
 
 if (doesModuleExist('jsx-a11y')) {
-  plugins.push('jsx-a11y')
-  extensions.push('plugin:jsx-a11y/recommended')
+    plugins.push('jsx-a11y')
+    extensions.push('plugin:jsx-a11y/recommended')
 }
 
 if (doesModuleExist('@tanstack/eslint-plugin-query')) {
-  plugins.push('@tanstack/query')
-  extensions.push('plugin:@tanstack/eslint-plugin-query/recommended')
+    plugins.push('@tanstack/query')
+    extensions.push('plugin:@tanstack/eslint-plugin-query/recommended')
 }
 
 const config: ESLint.ConfigData = {
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true
-    }
-  },
-  extends: extensions,
-  plugins,
-  rules: {},
-  settings: {
-    react: {
-      version: 'detect'
-    }
-  }
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+    },
+    extends: extensions,
+    plugins,
+    rules: {},
+    settings: {
+        react: {
+            version: 'detect',
+        },
+    },
 }
 
 export = config
