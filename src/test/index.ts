516453245aaa5af230a9953b3919ebdafbd5c51b
Ian McCausland <imccausl@gmail.com>
2024-02-20 19:40:18 -0500

    feat: add optional eslint-plugin-jest config

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,10 +1,12 @@
 import { doesModuleExist } from '../util'
 
-import type { ESLint } from 'eslint'
+import type { ESLint, Linter } from 'eslint'
 
 const overrides: ESLint.ConfigData['overrides'] = []
 const extensions: ESLint.ConfigData['extends'] = []
 const plugins: ESLint.ConfigData['plugins'] = []
+
+const doesJestPluginExist = doesModuleExist('eslint-plugin-jest')
 
 if (doesModuleExist('eslint-plugin-testing-library')) {
     extensions.push('plugin:testing-library/react')
@@ -15,15 +17,29 @@ if (doesModuleExist('eslint-plugin-vitest')) {
     plugins.push('vitest')
 }
 
+if (doesJestPluginExist) {
+    extensions.push('plugin:jest/recommended')
+    extensions.push('plugin:jest/style')
+    plugins.push('jest')
+}
+
 if (extensions.length || plugins.length) {
-    overrides.push({
+    const testConfig: Linter.ConfigOverride = {
         files: [
             '**/__tests__/**/*.[jt]s?(x)',
             '**/?(*.)+(spec|test).[jt]s?(x)',
         ],
         extends: extensions,
         plugins,
-    })
+    }
+
+    if (doesJestPluginExist) {
+        testConfig.env = {
+            jest: true,
+        }
+    }
+
+    overrides.push(testConfig)
 }
 
 const config: ESLint.ConfigData = {
