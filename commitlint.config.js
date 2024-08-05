b667033bb9b0ce0bea90c88a7b616d4decf8c1a5
Ian McCausland <imccausl@gmail.com>
2024-08-05 10:08:47 -0400

    chore: configure deps prefix for dependabot (#28)
    * chore: allow deps semantic commit
    
    * wip: configure commit prefix for dependabot

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,21 @@
 module.exports = {
   extends: [
     '@commitlint/config-conventional'
-  ]
+  ],
+  rules: {
+    'type-enum': [2, 'always', [
+      'build',
+      'chore',
+      'ci',
+      'deps',
+      'docs',
+      'feat',
+      'fix',
+      'perf',
+      'refactor',
+      'revert',
+      'style',
+      'test',
+    ]],
+  }
 }
