c9e96f3eae484592e94c5ad30d806ca097b3ba46
Ian McCausland <imccausl@gmail.com>
2024-09-02 20:22:17 -0400

    chore: use flat config

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+import { FlatCompat } from '@eslint/eslintrc'
+import js from '@eslint/js'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all,
+})
+
+export default [
+    ...compat.extends('@imccausl/eslint-config'),
+    {
+        languageOptions: {
+            ecmaVersion: 2021,
+            sourceType: 'script',
+
+            parserOptions: {
+                project: './tsconfig.json',
+            },
+        },
+    },
+]
