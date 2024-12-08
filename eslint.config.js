f6e03ad0d4e5a6132bd67d7884e1cdf52fba3121
Ian McCausland <imccausl@gmail.com>
2024-12-08 07:58:49 -0500

    wip: simplify config

--- eslint.config.js
+++ eslint.config.js
@@ -7,10 +7,5 @@ export default [
     ...eslintConfig,
     {
         files: ['src/**/*.ts'],
-        languageOptions: {
-            parserOptions: {
-                project: './tsconfig.json',
-            },
-        },
     },
 ]
