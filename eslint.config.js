59bb43fed87c80e69f6306c423051cfc150a89e5
Ian McCausland <imccausl@gmail.com>
2024-12-08 00:03:23 -0500

    wip: trying to make it work

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,23 @@
 import eslintConfig from '@imccausl/eslint-config'
 
 export default [
+  {
+    ignores: [
+      'lib/',
+      '.yarn/',
+      'node_modules/',
+      '.pnp.*'
+    ]
+  },
   ...eslintConfig,
   {
     files: [
       'src/**/*.ts',
-    ]
-  }
+    ],
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.json',
+      }
+    }
+  },
 ]
