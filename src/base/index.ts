169bfeea6e0e94136ab5ae25d13c8675a960aef2
Ian McCausland <imccausl@gmail.com>
2025-07-07 20:26:48 -0700

    wip: make linting work correctly / remove cruft

--- src/base/index.ts
+++ src/base/index.ts
@@ -2,6 +2,8 @@ import js from '@eslint/js'
 import importPlugin from 'eslint-plugin-import-x'
 import ts from 'typescript-eslint'
 
+import { importModuleIfExists } from '../util/index.js'
+
 import { languageOptions } from './languageOptions.js'
 import { rules } from './rules.js'
 import { settings } from './settings.js'
@@ -11,6 +13,7 @@ export default [
     ...ts.configs.recommended,
     ...ts.configs.stylistic,
     importPlugin.flatConfigs.recommended,
+    await importModuleIfExists('eslint-plugin-prettier/recommended'),
     {
         files: [
             '**/*.ts',
